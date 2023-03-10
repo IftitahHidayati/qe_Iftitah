8bcb242ddf8a2f1288ff208a72a53d7cc63289eb
IftitahHidayati <hidayatiiftitah@gmail.com>
2023-03-10 09:23:03 +0700

    Upload Tugas Session 7

--- 7_Basic Linux Command/Praktikum/Laporan Praktikum Session7.md
+++ 7_Basic Linux Command/Praktikum/Laporan Praktikum Session7.md
@@ -1,4 +1,4 @@
-# Tugas Session 7 - LinuxBasic Command
+# Tugas Session 7 - Basic Linux Command
 ## Link Pengerjaan Praktikum Tugas Session 7 :
 https://docs.google.com/document/d/1WiKsYFF2rMimU_UZTVv4WvBSwNFYpT8HqSYMZ7e6A2U/edit?usp=sharing
 
---
+++ 7_Basic Linux Command/Praktikum/myscript.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo 1. touch <nama file yg dibuat>
+echo 2. cp <nama file yg dibuat> <path direktori yg dituju>
+echo 3. cat > <nama file>
+echo 4. mv <nama file yg di move> <path folder yg dituju>
+echo 5. rm <nama file yg di hapus>
